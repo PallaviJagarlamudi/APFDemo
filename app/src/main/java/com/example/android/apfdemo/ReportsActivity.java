7700bf3e779227c056e7cfa70e02527ec909decb
PallaviJagarlamudi <pallavi.jagarlamudi36@gmail.com>
2017-03-09 02:10:01 +0530

    Added bar chart logic for Reports

--- app/src/main/java/com/example/android/apfdemo/ReportsActivity.java
+++ app/src/main/java/com/example/android/apfdemo/ReportsActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.apfdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
 
 public class ReportsActivity extends AppCompatActivity {
 
@@ -9,5 +16,28 @@ public class ReportsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
+
+        BarChart chart = (BarChart) findViewById(R.id.bar_chart);
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "# of defects resolved");
+
+        BarData data = new BarData(labels, dataset);
+        chart.setData(data);
+
     }
 }
